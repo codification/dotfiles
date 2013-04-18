2642502fdd8c1022f1a35b5acd7be6134949ed7e
Ville Svärd <ville.svard@transmode.com>
2013-04-18 11:04:51 +0200

    Added cleanup-buffer from emacs rocks and advice for sgml-delete-tag

--- .live-packs/visv-pack/config/magnars-defuns.el
+++ .live-packs/visv-pack/config/magnars-defuns.el
@@ -10,3 +10,30 @@
   (newline)
   (forward-line -1)
   (indent-for-tab-command))
+
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+
+(defun cleanup-buffer-safe ()
+  "Perform a bunch of safe operations on the whitespace content of a buffer.
+Does not indent buffer, because it is used for a before-save-hook, and that
+might be bad."
+  (interactive)
+  (delete-trailing-whitespace))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer.
+Including indent-buffer, which should not be called automatically on save."
+  (interactive)
+  (untabify-buffer)
+  (cleanup-buffer-safe)
+  (indent-buffer))
+
+(defadvice sgml-delete-tag (after reindent-buffer activate)
+  (cleanup-buffer))
