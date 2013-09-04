b7a2f16b1fa1929b0d0b30b88fc95ec3e4ced335
Ville Svärd <ville.svard@transmode.com>
2013-09-04 11:29:31 +0200

    Added use of temporary-file-directory for emacs save files

--- .live-packs/personal-pack/init.el
+++ .live-packs/personal-pack/init.el
@@ -71,3 +71,9 @@
     (lambda () (interactive) (find-alternate-file "..")))
   ; was dired-up-directory
  ))
+
+;; Temp files
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
